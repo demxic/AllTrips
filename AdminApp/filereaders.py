f9bd988ea86d45011817d154edf4919563bb7ace
demxic <demxic@gmail.com>
2019-03-02 22:05:37 -0600

    feat : Search for total trip numbers in the pbs.txt file

--- AdminApp/filereaders.py
+++ AdminApp/filereaders.py
@@ -1,5 +1,5 @@
 """This module holds functions needed to read pbs.txt files and turn them into schedule classes"""
-from AdminApp.adminregex import page_number_RE
+from AdminApp.adminregex import page_number_RE, trips_total_RE
 
 
 def verify_files(data_folder: str, file_names: list) -> list:
@@ -31,3 +31,10 @@ def page_number_remover(file: str) -> str:
         content = f.read()
         content_new = page_number_RE.sub(repl='', string=content)
     return content_new
+
+
+def number_of_trips_in_pbs_file(pbs_file_content: str) -> int:
+    """Searches for the 'Total number of trips' inside the pbs_file_content and returns the total number """
+    match_obj = trips_total_RE.search(pbs_file_content)
+    total_trips = int(match_obj.groupdict()['trips_total'])
+    return total_trips
