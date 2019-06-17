b0c8319db8c17ec3a78b40661946a45c4b680745
demxic <demxic@gmail.com>
2019-06-17 15:48:49 -0500

    refactor : Change the build_airport method keep track of all object loade

--- AdminApp/objectbuilders.py
+++ AdminApp/objectbuilders.py
@@ -1,11 +1,14 @@
 """This module contains functions that turn each dictionary into an object"""
-import datetime
-
 from AdminApp.exceptions import TripBlockError, UnbuiltTripError, DutyDayBlockError, UndefinedBlockTime, \
-    PreviouslyStoredTrip, UnsavedRoute, UnstoredTrip
+    PreviouslyStoredTrip, UnsavedRoute, UnstoredTrip, UnsavedAirport
 from models.scheduleclasses import Equipment, Route, Airport, Itinerary, Flight, DutyDay, Trip, Airline
 from models.timeclasses import DateTimeTracker
 import pytz
+
+# Set up these variables that will be used thru out
+airport = Airport(iata_code='MEX', timezone=pytz.timezone('America/Mexico_City'), viaticum='low_cost')
+route = Route(name='X', origin=airport, destination=airport, route_id=361)
+equipment = Equipment(airplane_code='789', cabin_members=9)
 
 
 def get_carrier_code(flight_dict: dict) -> str:
@@ -13,6 +16,41 @@ def get_carrier_code(flight_dict: dict) -> str:
     if flight_dict['equipment'].startswith(('D', 'E')):
         carrier_code = '6D'
     return carrier_code
+
+
+def build_airport(airport_iata_code: str) -> Airport:
+    try:
+        stored_airport = Airport.load_from_db(iata_code=airport_iata_code)
+    except UnsavedAirport as e:
+        print("Airport {} not saved in the database".format(e.airport_iata_code))
+        ans = input("Want to save it Y/N ? ").capitalize()
+        if ans == 'Y':
+            zone = input("Enter timezone ")
+            print("Choose a viaticum zone "
+                  "paris, new_york, usa, low_cost, "
+                  "madrid, high_cost, border")
+            viaticum_zone = input()
+            unstored_airport = Airport(iata_code=airport_iata_code,
+                                       timezone=pytz.timezone(zone=zone),
+                                       viaticum=viaticum_zone)
+            stored_airport = unstored_airport.save_to_db()
+        else:
+            raise e
+    return stored_airport
+
+
+def build_route(flight_name: str, origin: Airport, destination: Airport) -> Route:
+    try:
+        stored_route = Route.load_from_db(flight_name, origin, destination)
+    except UnsavedRoute as e:
+        print("Route {} not saved in the database".format(e.route))
+        ans = input("Want to save it Y/N? ").capitalize()
+        if ans == 'Y':
+            e.route.save_to_db()
+            stored_route = e.route
+        else:
+            raise e
+    return stored_route
 
 
 def build_itinerary(dt_tracker: DateTimeTracker, flight_dict: dict, suggested_blk: str) -> Itinerary:
@@ -30,55 +68,45 @@ def build_itinerary(dt_tracker: DateTimeTracker, flight_dict: dict, suggested_bl
 
 
 def build_flight(dt_tracker: DateTimeTracker, flight_dict: dict, postpone: bool, suggested_blk: str) -> Flight:
-    # # 1. Get required parameters
-    origin = Airport.load_from_db(iata_code=flight_dict['origin'])
-    destination = Airport.load_from_db(iata_code=flight_dict['destination'])
-    try:
-        route = Route.load_from_db(flight_dict['name'][-4:], origin, destination)
-    except UnsavedRoute as e:
-        print("Route {} not saved in the database".format(e.route))
-        ans = input("Want to save it Y/N ?").capitalize()
-        if ans == 'Y':
-            e.route.save_to_db()
-            route = e.route
-        else:
-            raise e
-
+    origin = build_airport(airport_iata_code=flight_dict['origin'])
+    destination = build_airport(airport_iata_code=flight_dict['destination'])
+    flight_route = build_route(flight_dict['name'][-4:], origin, destination)
     carrier_code = get_carrier_code(flight_dict=flight_dict)
-    equipment = Equipment.load_from_db(airplane_code=flight_dict['equipment'])
+    flight_equipment = Equipment.load_from_db(airplane_code=flight_dict['equipment'])
     itinerary = build_itinerary(dt_tracker=dt_tracker, flight_dict=flight_dict, suggested_blk=suggested_blk)
 
     # 1. Try loading the flight from the database
     loaded_flights = Flight.fetch_all_matching(airline_iata_code=carrier_code,
                                                scheduled_begin=itinerary._begin,
-                                               route=route)
-    flight = Flight(route=route, scheduled_itinerary=itinerary, equipment=equipment, carrier=carrier_code)
+                                               route=flight_route)
+    flight = Flight(route=flight_route, scheduled_itinerary=itinerary, equipment=flight_equipment,
+                    carrier=carrier_code)
 
     for loaded_flight in loaded_flights:
         loaded_flight.astimezone(timezone='local')
         flight.astimezone(timezone='local')
         if loaded_flight == flight:
             flight.event_id = loaded_flight.event_id
-        elif flight.begin.date() == loaded_flight.begin.date():
-            print("¿Cuál es el vuelo correcto?")
-            print("1 loaded  flight: ", loaded_flight)
-            print("2 created flight: ", flight)
-            print("3 ninguno ")
-            ans = input('¿1, 2 ó 3? : ')
-            if ans == '1':
+            flight.astimezone(timezone=pytz.utc)
+        elif not postpone:
+            print(80 * '*')
+            print("Which is the right flight? ")
+            print("1. {}".format(loaded_flight))
+            print("2. {}".format(flight))
+            print("3. None")
+            ans = int(input(': '))
+            if ans == 1:
                 flight = loaded_flight
                 flight.astimezone(timezone=pytz.utc)
                 dt_tracker.dt = flight.end
                 break
-            elif ans == '2':
-                flight.astimezone(timezone=pytz.utc)
-                flight.save_to_db()
+            elif ans == 2:
+                flight.astimezone(pytz.utc)
                 break
-        elif flight.duration.minutes == 0:
-            raise UndefinedBlockTime("No se ha podido deteriminar el blk time del vuelo {}/{}".format(
-                flight.name, flight.begin), flight_dict, flight)
+    else:
+        if flight.duration.minutes == 0 and not postpone:
+            raise UndefinedBlockTime(flight_dict=flight_dict, flight=flight)
 
-    flight.dh = not flight_dict['name'].isnumeric()
     return flight
     # if len(loaded_flights) >1:
     #     for loaded_flight in loaded_flights:
@@ -163,20 +191,22 @@ def build_duty_day(dt_tracker: DateTimeTracker, duty_day_dict: dict, postpone: b
         try:
             flight = build_flight(dt_tracker=dt_tracker, flight_dict=flight_dict, postpone=postpone,
                                   suggested_blk=duty_day_dict['crd'])
-            duty_day.append(flight)
-            dt_tracker.move(flight_dict['turn'])
         except UndefinedBlockTime as e:
-            if postpone:
-                raise UnbuiltTripError()
-            else:
-                print("FLT {} {} {} {} {} {} ".format(dt_tracker.date, e.flight_dict['name'],
-                                                      e.flight_dict['origin'], e.flight_dict['begin'],
-                                                      e.flight_dict['destination'], e.flight_dict['end']))
-                print("unable to determine DH time.")
-                print("")
-                blk = input("Insert time as HHMM format :")
-                td = dt_tracker.move(blk)
-                itinerary = Itinerary.from_timedelta(begin=e.dt_tracker, a_timedelta=td)
+            print(80 * "*")
+            print("FLT {} {} {} {} {} {} ".format(dt_tracker.date, e.flight_dict['name'],
+                                                  e.flight_dict['origin'], e.flight_dict['begin'],
+                                                  e.flight_dict['destination'], e.flight_dict['end']))
+            print("unable to determine DH time.")
+            blk = input("Insert DH time as HHMM format: ")
+            print("")
+
+            td = dt_tracker.move(blk)
+            itinerary = Itinerary.from_timedelta(begin=e.flight.begin, a_timedelta=td)
+            e.flight.scheduled_itinerary = itinerary
+            flight = e.flight
+        flight.dh = not flight_dict['name'].isnumeric()
+        duty_day.append(flight)
+        dt_tracker.move(flight_dict['turn'])
     dt_tracker.move(time_string='00:30')
     dt_tracker.move(duty_day_dict['layover_duration'])
 
@@ -188,13 +218,14 @@ def build_duty_day(dt_tracker: DateTimeTracker, duty_day_dict: dict, postpone: b
 
 
 def build_trip(trip_dict: dict, postpone: bool) -> Trip:
-    dt_tracker = DateTimeTracker(trip_dict['date_and_time'], timezone=pytz.timezone('America/Mexico_City'))
+    trip_base = Airport.load_from_db(trip_dict['trip_base'])
+    dt_tracker = DateTimeTracker(trip_dict['date_and_time'], timezone=trip_base.timezone)
     try:
         trip = Trip.load_trip_info(trip_number=trip_dict['number'], dated=dt_tracker.date)
     except UnstoredTrip:
         trip = Trip(number=trip_dict['number'], dated=dt_tracker.date,
                     crew_position=trip_dict['crew_position'],
-                    crew_base=Airport.load_from_db(iata_code=trip_dict['crew_base']))
+                    trip_base=trip_base)
         dt_tracker.change_to_timezone(pytz.utc)
 
         for json_dd in trip_dict['duty_days']:
@@ -211,7 +242,14 @@ def build_trip(trip_dict: dict, postpone: bool) -> Trip:
                     raise UnbuiltTripError
                 else:
                     print("... Correcting for inconsistent duty day: ")
-                    e.correct_invalid_events()
+                    for flight in e.duty_day.events:
+                        print(flight)
+                        r = input("Is flight properly built? y/n ").capitalize()
+                        if 'N' in r:
+                            itinerary_string = input("Enter itinerary as string (date, begin, blk) 31052018 2206 0122 ")
+                            itinerary = Itinerary.from_string(itinerary_string)
+                            flight.scheduled_itinerary = itinerary
+                            flight.update_to_db()
                     print("Corrected duty day")
                     print(e.duty_day)
                     trip.append(e.duty_day)
@@ -223,17 +261,15 @@ def build_trip(trip_dict: dict, postpone: bool) -> Trip:
     return trip
 
 
-def create_trips(trips_as_dict: list, position: str, postpone: bool = True) -> list:
+def create_trips(trips_as_dict: list, postpone: bool = True) -> list:
     # 2. Turn each trip_dict into a Trip object
     built_trips_count: int = 0
     unbuilt_trips = list()
     for trip_dict in trips_as_dict:
-        # if 'position' not in trip_dict:
-        #     trip_dict['position'] = position
         try:
-            # if trip_dict['number'] == '5148':
+            # if trip_dict['number'] == '6384':
             #     print("found trip {}".format(trip_dict['number']))
-            trip = build_trip(trip_dict, postpone)
+            trip = build_trip(trip_dict=trip_dict, postpone=postpone)
 
         except TripBlockError as e:
             # TODO : Granted, there's a trip block error, what actions should be taken to correct it? (missing)
@@ -247,7 +283,6 @@ def create_trips(trips_as_dict: list, position: str, postpone: bool = True) -> l
             unbuilt_trips.append(trip_dict)
 
         else:
-            trip.position = position
             trip.astimezone('local')
             # print(trip)
             trip.astimezone(pytz.utc)
